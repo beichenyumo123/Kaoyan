7ddcd65e3676989dca9d4f1fb0defd89f32c6252
beichenyumo123 <15713775462@139.com>
2026-04-14 21:28:42 +0800

    考研交流论坛后端初始化

---
+++ src/main/java/com/zzu/kaoyan/KaoyanApplication.java
@@ -0,0 +1,13 @@
+package com.zzu.kaoyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaoyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaoyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zzu/kaoyan/KaoyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zzu.kaoyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaoyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
