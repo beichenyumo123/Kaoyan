580446f8831cbeace58684e5579ec16d19e4bb3f
LucidRift-stack <1956453019@qq.com>
2026-04-17 13:19:54 +0800

    完成私信模块开发

---
+++ src/main/java/com/zzu/kaoyan/module/message/mapper/MessageMapper.java
@@ -0,0 +1,9 @@
+package com.zzu.kaoyan.module.message.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zzu.kaoyan.module.message.entity.Message;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MessageMapper extends BaseMapper<Message> {
+}
