38bcccfed5a6d31939b11a8ae222b28ce309ebf3
beichenyumo123 <15713775462@139.com>
2026-04-18 19:09:05 +0800

    删除多余数据库

--- src/main/resources/V1.0.1_alter_id_Auto_increment.sql
+++
@@ -1,8 +0,0 @@
---新增 forum_post的id自增
-ALTER TABLE forum_post MODIFY COLUMN id INT AUTO_INCREMENT;
---新增 forum_board的id自增
-ALTER TABLE forum_board MODIFY COLUMN id INT AUTO_INCREMENT;
---为forum_post增加tags
-ALTER TABLE forum_post
-    ADD COLUMN tags JSON NULL COMMENT '标签列表'
-        AFTER content;
--- src/main/resources/kaoyan_forum_v2.sql
+++ src/main/resources/kaoyan_forum_v2.sql
@@ -11,12 +11,8 @@
  Target Server Version : 90300 (9.3.0)
  File Encoding         : 65001
 
- Date: 18/04/2026 11:24:38
+ Date: 18/04/2026 19:08:08
 */
-
-/*
- 目前适配最新的整合分支v1
- */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -60,7 +56,7 @@ CREATE TABLE `forum_comment`  (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_post_id`(`post_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2045336144836669442 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子评论表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2045435015252168706 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子评论表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of forum_comment
@@ -78,6 +74,22 @@ INSERT INTO `forum_comment` VALUES (2045335555956387842, 5, 6, NULL, '？？', 0
 INSERT INTO `forum_comment` VALUES (2045335583622017026, 5, 6, NULL, '我的用户名呢？', 0, '2026-04-18 10:56:18', '2026-04-18 10:56:18');
 INSERT INTO `forum_comment` VALUES (2045335628350074881, 1, 6, NULL, '用户名呢', 0, '2026-04-18 10:56:29', '2026-04-18 10:56:29');
 INSERT INTO `forum_comment` VALUES (2045336144836669441, 1, 6, NULL, 'NB', 0, '2026-04-18 10:58:32', '2026-04-18 10:58:32');
+INSERT INTO `forum_comment` VALUES (2045351143416119298, 5, 6, NULL, 'nb', 0, '2026-04-18 11:58:08', '2026-04-18 11:58:08');
+INSERT INTO `forum_comment` VALUES (2045352270488518657, 5, 6, NULL, '牛逼', 0, '2026-04-18 12:02:37', '2026-04-18 12:02:37');
+INSERT INTO `forum_comment` VALUES (2045376997667979265, 6, 6, NULL, 'nb', 0, '2026-04-18 13:40:52', '2026-04-18 13:40:52');
+INSERT INTO `forum_comment` VALUES (2045381396712734721, 6, 6, NULL, 'nb', 0, '2026-04-18 13:58:21', '2026-04-18 13:58:21');
+INSERT INTO `forum_comment` VALUES (2045384489915404289, 7, 6, NULL, 'nb', 0, '2026-04-18 14:10:38', '2026-04-18 14:10:38');
+INSERT INTO `forum_comment` VALUES (2045384511004364802, 7, 6, 2045384489915404289, 'nb', 0, '2026-04-18 14:10:43', '2026-04-18 14:10:43');
+INSERT INTO `forum_comment` VALUES (2045384648929857537, 6, 6, 2045376997667979265, 'nb', 0, '2026-04-18 14:11:16', '2026-04-18 14:11:16');
+INSERT INTO `forum_comment` VALUES (2045385386498215938, 7, 6, 2045384489915404289, 'NB', 0, '2026-04-18 14:14:12', '2026-04-18 14:14:12');
+INSERT INTO `forum_comment` VALUES (2045385414990123009, 7, 6, 2045384489915404289, '吃回复', 0, '2026-04-18 14:14:19', '2026-04-18 14:14:19');
+INSERT INTO `forum_comment` VALUES (2045386841577771009, 7, 6, 2045384511004364802, 'NB', 0, '2026-04-18 14:19:59', '2026-04-18 14:19:59');
+INSERT INTO `forum_comment` VALUES (2045390912309186561, 7, 6, 2045384511004364802, 'NB', 0, '2026-04-18 14:36:10', '2026-04-18 14:36:10');
+INSERT INTO `forum_comment` VALUES (2045390931523293186, 7, 6, 2045390912309186561, 'NB', 0, '2026-04-18 14:36:14', '2026-04-18 14:36:14');
+INSERT INTO `forum_comment` VALUES (2045396380482031617, 7, 6, 2045384489915404289, 'NB', 0, '2026-04-18 14:57:53', '2026-04-18 14:57:53');
+INSERT INTO `forum_comment` VALUES (2045418841785257985, 7, 6, 2045384489915404289, 'nb', 0, '2026-04-18 16:27:09', '2026-04-18 16:27:09');
+INSERT INTO `forum_comment` VALUES (2045435002438569986, 7, 6, 2045384511004364802, 'NB', 0, '2026-04-18 17:31:22', '2026-04-18 17:31:22');
+INSERT INTO `forum_comment` VALUES (2045435015252168705, 7, 6, NULL, 'NB', 0, '2026-04-18 17:31:25', '2026-04-18 17:31:25');
 
 -- ----------------------------
 -- Table structure for forum_post
@@ -100,16 +112,40 @@ CREATE TABLE `forum_post`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_board_id`(`board_id` ASC) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子主表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子主表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of forum_post
 -- ----------------------------
-INSERT INTO `forum_post` VALUES (1, 1, 3, '2026年408复习规划（个人向分享）', '<p>大家好，这是我的408复习规划，基础阶段建议看王道，强化阶段多刷真题...</p><p>附上了我的复习时间表，供大家参考！</p>', '[\"408\", \"复习规划\", \"经验贴\"]', '[\"https://oss.example.com/file/plan.pdf\"]', 1513, 3, 3, 0, '2026-04-17 20:44:07', '2026-04-18 11:07:36');
-INSERT INTO `forum_post` VALUES (2, 2, 4, '求助！张宇18讲的线代部分看不懂怎么办？', '<p>特别是特征值和特征向量那一部分，做题完全没有思路，感觉非常吃力，求大佬指点迷津！</p>', '[\"数学一\", \"线性代数\", \"求助\"]', '[]', 327, 0, 1, 0, '2026-04-17 20:44:07', '2026-04-18 10:55:24');
+INSERT INTO `forum_post` VALUES (1, 1, 3, '2026年408复习规划（个人向分享）', '<p>大家好，这是我的408复习规划，基础阶段建议看王道，强化阶段多刷真题...</p><p>附上了我的复习时间表，供大家参考！</p>', '[\"408\", \"复习规划\", \"经验贴\"]', '[\"https://oss.example.com/file/plan.pdf\"]', 1527, 3, 3, 0, '2026-04-17 20:44:07', '2026-04-18 19:01:49');
+INSERT INTO `forum_post` VALUES (2, 2, 4, '求助！张宇18讲的线代部分看不懂怎么办？', '<p>特别是特征值和特征向量那一部分，做题完全没有思路，感觉非常吃力，求大佬指点迷津！</p>', '[\"数学一\", \"线性代数\", \"求助\"]', '[]', 330, 1, 1, 0, '2026-04-17 20:44:07', '2026-04-18 18:15:38');
 INSERT INTO `forum_post` VALUES (3, 3, 5, '分享一份自己整理的英语大小作文万能句型', '<p>背熟这些句型，考试的时候直接套用，亲测有效！详见附件下载，完全免费分享给大家~</p>', '[\"英语一\", \"作文\", \"干货\"]', '[\"https://oss.example.com/file/english_writing.docx\"]', 2000, 2, 0, 0, '2026-04-17 20:44:07', '2026-04-17 20:44:07');
-INSERT INTO `forum_post` VALUES (4, 4, 2, '肖秀荣1000题刷题打卡集中贴（2026版）', '<p>欢迎大家每天在这里打卡自己的刷题进度，互相监督，共同进步！每天坚持100道题！</p>', '[\"政治\", \"打卡\", \"官方贴\"]', '[]', 500, 0, 0, 0, '2026-04-17 20:44:07', '2026-04-17 20:44:07');
-INSERT INTO `forum_post` VALUES (5, 1, 6, '测试', '测试', '[]', NULL, 10, 1, 0, 0, '2026-04-17 22:28:18', '2026-04-18 11:19:23');
+INSERT INTO `forum_post` VALUES (4, 4, 2, '肖秀荣1000题刷题打卡集中贴（2026版）', '<p>欢迎大家每天在这里打卡自己的刷题进度，互相监督，共同进步！每天坚持100道题！</p>', '[\"政治\", \"打卡\", \"官方贴\"]', '[]', 501, 0, 0, 0, '2026-04-17 20:44:07', '2026-04-18 15:24:39');
+INSERT INTO `forum_post` VALUES (5, 1, 6, '测试', '测试', '[]', NULL, 16, 1, 0, 0, '2026-04-17 22:28:18', '2026-04-18 18:30:46');
+INSERT INTO `forum_post` VALUES (6, 2, 6, '测试', '测试', '[]', NULL, 11, 1, 0, 0, '2026-04-18 12:02:59', '2026-04-18 18:47:39');
+INSERT INTO `forum_post` VALUES (7, 1, 6, '测试2', '测试2', '[]', NULL, 30, 1, 0, 0, '2026-04-18 12:11:10', '2026-04-18 18:28:23');
+INSERT INTO `forum_post` VALUES (8, 1, 6, '测试', '测试', '[]', NULL, 19, 1, 0, 0, '2026-04-18 17:07:34', '2026-04-18 18:58:35');
+INSERT INTO `forum_post` VALUES (9, 1, 6, '测试', '测试', '[]', NULL, 19, 1, 0, 0, '2026-04-18 18:04:30', '2026-04-18 19:02:55');
+
+-- ----------------------------
+-- Table structure for forum_post_collect
+-- ----------------------------
+DROP TABLE IF EXISTS `forum_post_collect`;
+CREATE TABLE `forum_post_collect`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `post_id` bigint NOT NULL COMMENT '帖子ID',
+  `user_id` bigint NOT NULL COMMENT '收藏用户ID',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '收藏时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_post_user`(`post_id` ASC, `user_id` ASC) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子收藏关联表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of forum_post_collect
+-- ----------------------------
+INSERT INTO `forum_post_collect` VALUES (6, 1, 6, '2026-04-18 18:47:53');
+INSERT INTO `forum_post_collect` VALUES (7, 8, 6, '2026-04-18 18:48:46');
+INSERT INTO `forum_post_collect` VALUES (8, 9, 6, '2026-04-18 18:49:24');
 
 -- ----------------------------
 -- Table structure for forum_post_like
@@ -122,7 +158,7 @@ CREATE TABLE `forum_post_like`  (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '点赞时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_post_user`(`post_id` ASC, `user_id` ASC) USING BTREE COMMENT '防止重复点赞'
-) ENGINE = InnoDB AUTO_INCREMENT = 2045333316848168962 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子点赞关联表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2045443773990727682 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '帖子点赞关联表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of forum_post_like
@@ -133,6 +169,11 @@ INSERT INTO `forum_post_like` VALUES (3, 3, 3, '2026-04-17 20:44:08');
 INSERT INTO `forum_post_like` VALUES (4, 3, 4, '2026-04-17 20:44:08');
 INSERT INTO `forum_post_like` VALUES (2045122250729869314, 1, 6, '2026-04-17 20:48:36');
 INSERT INTO `forum_post_like` VALUES (2045333316848168961, 5, 6, '2026-04-18 10:47:18');
+INSERT INTO `forum_post_like` VALUES (2045428795762036738, 7, 6, '2026-04-18 17:06:42');
+INSERT INTO `forum_post_like` VALUES (2045431325170618370, 8, 6, '2026-04-18 17:16:45');
+INSERT INTO `forum_post_like` VALUES (2045443401733664770, 9, 6, '2026-04-18 18:04:44');
+INSERT INTO `forum_post_like` VALUES (2045443684639469570, 6, 6, '2026-04-18 18:05:52');
+INSERT INTO `forum_post_like` VALUES (2045443773990727681, 2, 6, '2026-04-18 18:06:13');
 
 -- ----------------------------
 -- Table structure for forum_report
@@ -168,7 +209,7 @@ CREATE TABLE `interaction_check_in`  (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '具体打卡时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_user_date`(`user_id` ASC, `created_date` ASC) USING BTREE COMMENT '每天每人只能打卡一次'
-) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '每日学习打卡表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '每日学习打卡表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of interaction_check_in
@@ -177,6 +218,7 @@ INSERT INTO `interaction_check_in` VALUES (1, 3, 8, '今天完成了数据结构
 INSERT INTO `interaction_check_in` VALUES (2, 4, 10, '高数刷了50题，英语背了100个单词，充实的一天！继续保持！', '2026-04-16', '2026-04-17 20:44:08');
 INSERT INTO `interaction_check_in` VALUES (3, 5, 6, '今天有点感冒，状态不好，只背了2个单元的政治，明天要补回来。', '2026-04-16', '2026-04-17 20:44:08');
 INSERT INTO `interaction_check_in` VALUES (4, 3, 9, '计组真题第一套完成，大题还要加强，感觉指令系统那块还是不熟。', '2026-04-17', '2026-04-17 20:44:08');
+INSERT INTO `interaction_check_in` VALUES (5, 6, 24, '状态极佳，暴力学习24小时', '2026-04-18', '2026-04-18 11:42:38');
 
 -- ----------------------------
 -- Table structure for interaction_points_log
@@ -192,11 +234,12 @@ CREATE TABLE `interaction_points_log`  (
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_user_id`(`user_id` ASC) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '积分变动日志表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '积分变动日志表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of interaction_points_log
 -- ----------------------------
+INSERT INTO `interaction_points_log` VALUES (1, 6, 3, 'CHECK_IN', NULL, '打卡+3分', '2026-04-18 11:42:38');
 
 -- ----------------------------
 -- Table structure for interaction_user_study
@@ -211,11 +254,12 @@ CREATE TABLE `interaction_user_study`  (
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_user_id`(`user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户学习打卡统计' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户学习打卡统计' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of interaction_user_study
 -- ----------------------------
+INSERT INTO `interaction_user_study` VALUES (1, 6, 1, 1, '2026-04-18', '2026-04-18 11:42:38');
 
 -- ----------------------------
 -- Table structure for message
@@ -231,7 +275,7 @@ CREATE TABLE `message`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `idx_from_user`(`from_user_id` ASC) USING BTREE,
   INDEX `idx_to_user`(`to_user_id` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 40 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '私信表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 45 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '私信表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of message
@@ -275,6 +319,11 @@ INSERT INTO `message` VALUES (36, 7, 6, '特娘的', 1, NULL);
 INSERT INTO `message` VALUES (37, 6, 7, 'NB', 1, NULL);
 INSERT INTO `message` VALUES (38, 7, 6, 'NB', 1, NULL);
 INSERT INTO `message` VALUES (39, 6, 3, '学长在吗', 0, NULL);
+INSERT INTO `message` VALUES (40, 6, 7, 'NB', 1, NULL);
+INSERT INTO `message` VALUES (41, 6, 3, 'NB', 0, NULL);
+INSERT INTO `message` VALUES (42, 6, 3, '喂喂喂', 0, NULL);
+INSERT INTO `message` VALUES (43, 7, 6, '发什么什么事了？', 1, NULL);
+INSERT INTO `message` VALUES (44, 6, 7, '收到', 1, NULL);
 
 -- ----------------------------
 -- Table structure for resource_file
@@ -333,7 +382,42 @@ INSERT INTO `sys_user` VALUES (2, '考研高数版主', '$2a$10$7JB720yubVSZvUI0
 INSERT INTO `sys_user` VALUES (3, '408上岸人', '$2a$10$7JB720yubVSZvUI0rEqK/.VqGOZTH.ulu33dHOiBE8ByOhJIrdAu2', 'user1@example.com', '13800000003', 'USER', 'https://dummyimage.com/100x100/28a745/fff&text=U1', '计算机科学与技术', 150, 0, '2026-04-17 20:44:07', '2026-04-17 20:44:07');
 INSERT INTO `sys_user` VALUES (4, '英语困难户', '$2a$10$7JB720yubVSZvUI0rEqK/.VqGOZTH.ulu33dHOiBE8ByOhJIrdAu2', 'user2@example.com', '13800000004', 'USER', 'https://dummyimage.com/100x100/dc3545/fff&text=U2', '金融学', 200, 0, '2026-04-17 20:44:07', '2026-04-17 20:44:07');
 INSERT INTO `sys_user` VALUES (5, '政治背书狂', '$2a$10$7JB720yubVSZvUI0rEqK/.VqGOZTH.ulu33dHOiBE8ByOhJIrdAu2', 'user3@example.com', '13800000005', 'USER', 'https://dummyimage.com/100x100/ffc107/000&text=U3', '法学', 300, 0, '2026-04-17 20:44:07', '2026-04-17 20:44:07');
-INSERT INTO `sys_user` VALUES (6, 'DB886', '$2a$10$PYWXnjNRZ0qPWpYwQdU2aO.VKorN3YQyAhoof8D4E.vp5DVNWAdsu', '1@qq.com', '15713775462', 'USER', '', '408', 0, 0, '2026-04-17 20:46:42', '2026-04-17 20:46:42');
+INSERT INTO `sys_user` VALUES (6, 'DB', '$2a$10$PYWXnjNRZ0qPWpYwQdU2aO.VKorN3YQyAhoof8D4E.vp5DVNWAdsu', '1@qq.com', '15713775462', 'USER', '', '408', 3, 0, '2026-04-17 20:46:42', '2026-04-18 11:42:38');
 INSERT INTO `sys_user` VALUES (7, '吴东博', '$2a$10$DMcxnrmr8prWhiQxc5UmAuvtd0nVVv6gk.ASAvXP8B.HWIKEBHCTm', '2@qq.com', NULL, 'USER', NULL, NULL, 0, 0, '2026-04-17 22:01:13', '2026-04-17 22:01:13');
+
+-- ----------------------------
+-- Table structure for sys_user_stats
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_user_stats`;
+CREATE TABLE `sys_user_stats`  (
+  `user_id` bigint NOT NULL COMMENT '关联 sys_user 表的主键 ID',
+  `post_count` int NOT NULL DEFAULT 0 COMMENT '发帖总数',
+  `like_received_count` int NOT NULL DEFAULT 0 COMMENT '获得的获赞总数',
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci COMMENT = '用户数据统计表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of sys_user_stats
+-- ----------------------------
+INSERT INTO `sys_user_stats` VALUES (2, 1, 0, '2026-04-18 18:15:26');
+INSERT INTO `sys_user_stats` VALUES (3, 1, 3, '2026-04-18 18:15:26');
+INSERT INTO `sys_user_stats` VALUES (4, 1, 1, '2026-04-18 18:15:26');
+INSERT INTO `sys_user_stats` VALUES (5, 1, 2, '2026-04-18 18:15:26');
+INSERT INTO `sys_user_stats` VALUES (6, 5, 5, '2026-04-18 18:15:26');
+
+-- ----------------------------
+-- Triggers structure for table forum_post
+-- ----------------------------
+DROP TRIGGER IF EXISTS `trg_forum_post_after_insert`;
+delimiter ;;
+CREATE TRIGGER `trg_forum_post_after_insert` AFTER INSERT ON `forum_post` FOR EACH ROW BEGIN
+    INSERT INTO sys_user_stats (user_id, post_count, like_received_count)
+    VALUES (NEW.user_id, 1, 0)
+    ON DUPLICATE KEY UPDATE
+        post_count = post_count + 1;
+END
+;;
+delimiter ;
 
 SET FOREIGN_KEY_CHECKS = 1;
