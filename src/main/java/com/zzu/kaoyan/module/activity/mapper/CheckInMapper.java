77e6c821a51902f98cc426744dcffb7a214f0dc5
Moeie228 <209384728@qq.com>
2026-04-18 11:39:11 +0800

    11点38

--- src/main/java/com/zzu/kaoyan/module/activity/mapper/CheckInMapper.java
+++ src/main/java/com/zzu/kaoyan/module/activity/mapper/CheckInMapper.java
@@ -3,8 +3,10 @@ package com.zzu.kaoyan.module.activity.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zzu.kaoyan.module.activity.entity.po.CheckInPO;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import java.time.LocalDate;
 
 public interface CheckInMapper extends BaseMapper<CheckInPO> {
+    @Select("SELECT COUNT(*) > 0 FROM interaction_check_in WHERE user_id = #{userId} AND created_date = #{date}")
     boolean existsByUserAndDate(@Param("userId") Long userId, @Param("date") LocalDate date);
 }
